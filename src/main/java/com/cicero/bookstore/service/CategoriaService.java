132779ff14e414ce1c8bc98d27384d8c642a5be2
Cicero Calisto <cicero.calisto97@gmail.com>
2022-02-18 15:37:22 -0300

    Excecao Personalizada para um objeto nao encontrado

--- src/main/java/com/cicero/bookstore/service/CategoriaService.java
+++ src/main/java/com/cicero/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.cicero.bookstore.domain.Categoria;
 import com.cicero.bookstore.repositories.CategoriaRepository;
+import com.cicero.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: "+ Categoria.class.getName()));
 	}
 	
 }
